5ddf155ee6d4f5ba9c834d43a2653ab68260c7b2
Tanu-7827 <tanishkaaggarwal2005@gmail.com>
2026-06-25 18:35:06 +0530

    Added to classwork file

---
+++ Classwork/Day 1 Presentation_25th_june/Difference of two numbers.py
@@ -0,0 +1,28 @@
+''' To calculate the difference of two numbers '''
+
+# Input of first number from the user
+num1 = float(input("Enter the first number: "))
+
+# Input of second number from the user
+num2 = float(input("Enter the second number: "))
+
+# -----------------------------------------------------
+
+# Displaying data to the user
+print("First Number:", num1)
+print("Second Number:", num2)
+
+# -----------------------------------------------------
+
+# Calculating the difference
+difference = num1 - num2
+
+# Displaying the difference
+print("Difference of the two numbers:", difference)
+'''output:
+Enter the first number: 786
+Enter the second number: 453
+First Number: 786.0
+Second Number: 453.0
+Difference of the two numbers: 333.0
+'''
